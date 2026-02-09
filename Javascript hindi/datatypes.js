81c689fbfb6a74d351933afc3ccfd9f5ca4cddaa
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-09 16:18:07 +0530

    datatypes done

---
+++ Javascript hindi/datatypes.js
@@ -0,0 +1,18 @@
+let state;
+let path = null;
+
+console.log("3+3");
+console.log("Abhinav");
+
+//number => 123, 3.14, -456, Infinity, -Infinity, NaN
+//bigint
+//string => "", '', `` (template literals)
+//boolean => true, false
+//undefined => undefined
+//null => null
+//symbol => Symbol("description")
+
+let age = 25;   
+
+console.log(typeof age); // Output: "number"
+console.log(typeof path); // Output: "undefined"
