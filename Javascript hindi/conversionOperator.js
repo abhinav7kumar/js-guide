46a84d37295afffb23028aa46b7015b2cb88f07a
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-10 16:27:45 +0530

    conversion operator phase 2

--- Javascript hindi/conversionOperator.js
+++ Javascript hindi/conversionOperator.js
@@ -1,3 +1,5 @@
+// ***************** Conversion Operator in JavaScript*****************
+
 let score = 33;
 
 
@@ -19,3 +21,37 @@ console.log(bolleanIsLoggedIn);
 // 1 => true
 // 0 => false
 // "" => false
+
+let SumNumber = 33
+
+let SumString = String(SumNumber);
+
+console.log(SumString);
+console.log(typeof SumString);
+
+
+// *********************** Operators in JavaScript***********************
+
+let value = 10;
+let negValue = -value; // Negation
+console.log(-value); 
+
+let str1 = "Hello, ";
+let str2 = "world!";
+let concatenatedStr = str1 + str2;
+console.log(concatenatedStr);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+console.log(typeof gameCounter);
+
+let a= 5;
+b = a++;
+console.log(a);
+
+let x = 5;
+let y = 10;
+
+console.log(x+y); // false
+console .log(x<y); // -5
