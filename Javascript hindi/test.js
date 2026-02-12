aca3d415332ac19b5ef59d27840bd2e9bc0a152b
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-12 21:36:19 +0530

    implement comparison operator examples

--- Javascript hindi/test.js
+++ Javascript hindi/test.js
@@ -30,3 +30,16 @@ console.log(city);
 console.log(country);
 console.log(profession);
 
+
+// Comparison Operators
+
+let a = 10;
+let b = "10";
+
+console.log(a == b);   // true (value only)
+console.log(a === b);  // false (value + type)
+console.log(a != b);   // false
+console.log(a !== b);  // true
+console.log(a > 5);    // true
+console.log(a <= 10);  // true
+
