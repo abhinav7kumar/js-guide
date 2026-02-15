f5eeb5de2ba5c50c6ee23db4aa8a951f6a2de595
Abhinav Kumar <abhinav.kumar9888@gmail.com>
2026-02-15 17:18:28 +0530

    new message

---
+++ Javascript hindi/num_and_maths.js
@@ -0,0 +1,12 @@
+const score = 100;
+
+const balanace = new Number(5000);
+console.log(balanace);
+
+console.log(balanace.toString());
+console.log(typeof(balanace));
+console.log(balanace.toFixed(2));
+
+doll = '$' + balanace;
+console.log(doll);
+
